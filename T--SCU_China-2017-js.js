26f14b9a2b5d62cf82b515c558058dae66287744
maicius <maicius@outlook.com>
2017-10-13 14:30:36 +0800

    Finish some animates

--- T--SCU_China-2017-js.js
+++ T--SCU_China-2017-js.js
@@ -2,21 +2,26 @@
  * Created by maicius on 2017/9/26.
  */
 
-function click_scroll() {
-    console.log("enter");
-    var scroll_offset = $("#scu_project").offset();
-    $("body,html").animate({
-        scrollTop: scroll_offset.top
-    }, 1000);
-}
-
 $(document).ready(function () {
-        console.log(document.body.scrollTop);
-        if (document.body.scrollTop > 700) {
-            $('#scu_down_image').style.opacity = 0
-        }
-    }
-);
+        $(window).scroll(function () {
+
+            // 隐藏侧边导航栏
+            console.log($(window).scrollTop());
+            if($(window).scrollTop() >= 200) {
+                $('#scu_nav_side').css("margin-left", "0px");
+            }else {
+                $('#scu_nav_side').css("margin-left", "-700px");
+            }
+
+            //隐藏下拉按钮
+            if ($(window).scrollTop() >= 500) {
+                $('#scu_down').css("opacity", "0.0");
+            }
+            else{
+                $('#scu_down').css("opacity", "1.0");
+            }
+        })
+});
 
 $('#scu_down_image').bind('mousewheel', function (event, delta, deltaX, deltaY) {
     console.log(delta, deltaX, deltaY);
